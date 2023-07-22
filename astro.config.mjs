3d866272b52691a3af31438750a7344bd16e3970
Cbrnex <31788262+7rs@users.noreply.github.com>
2023-07-23 01:02:37 +0900

    Add main page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [image(), tailwind(), compress()]
+});
