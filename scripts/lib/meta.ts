b8b22985dd61e13f1ba0b836993641d179ed599f
Cbrnex <31788262+7rs@users.noreply.github.com>
2024-01-08 19:41:39 +0900

    Added meta layout

---
+++ scripts/lib/meta.ts
@@ -0,0 +1,14 @@
+export type Metadata = {
+    name: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    author: string;
+    url: string;
+    keywords: Array<string>;
+    twitterId?: string;
+}
+
+export function createFontURL(fonts: Array<string>): string {
+    return `https://fonts.googleapis.com/css2?family=${fonts.join("&family=").replace(/\s/g, "+")}&display=swap`;
+}
